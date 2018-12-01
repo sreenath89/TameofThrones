cbeea6e4985e99e7b23b7d8b2654def3dea34a60
Sreenath M Menon <31819677+sreenathmmenon@users.noreply.github.com>
2018-12-01 23:11:15 +0530

    Changes to build file
    Changes to build file

--- sample_build.sh
+++ sample_build.sh
@@ -2,6 +2,8 @@
 
 git clone https://github.com/sreenathmmenon/TameofThrones
 cd TameofThrones
-cd tameofthrones
-python test_tameofthrones.py -v
-python tameofthrones.py
+pip install flake8 pytest pytest-cov
+python -m unittest -v tameofthrones.test_tameofthrones
+flake8 --exclude=venv* --statistics
+pytest -v --cov=tameofthrones
+python tameofthrones/tameofthrones.py
