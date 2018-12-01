cb68dc8cb57d14f687c3d57ab73caedc4e4ec45b
Sreenath <zreenathmenon@gmail.com>
2018-12-01 23:20:37 +0530

    Adding steps for execution from a zip file./

---
+++ sample_build_from_zip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd TameofThrones
+pip install flake8 pytest pytest-cov
+python -m unittest -v tameofthrones.test_tameofthrones
+flake8 --exclude=venv* --statistics
+pytest -v --cov=tameofthrones
+python tameofthrones/tameofthrones.py
