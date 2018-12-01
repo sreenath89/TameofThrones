efe5ecc7a0938e6dce1f725c80f65645222aaa9e
Sreenath <zreenathmenon@gmail.com>
2018-12-01 14:29:19 +0530

    Fixing errors found during testing

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
 setup(
-        name='TameofThrones', 
-        version='1.0', 
-        packages=['tameofthrones',], 
-        license='Apache 2.0', 
+        name='TameofThrones',
+        version='1.0',
+        packages=['tameofthrones', ],
+        license='Apache 2.0',
         # long_description=open('README.rst').read(),
 )
+
